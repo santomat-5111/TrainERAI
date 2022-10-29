af6837516f6a9e444fa9c0f33c9a8bdca0d99e0d
marzuk <you@example.com>
2022-10-29 07:30:11 +0530

    added calorie suggester

--- static/js/bmi.js
+++ static/js/bmi.js
@@ -20,35 +20,99 @@ function validateForm() {
 document.getElementById("submit").addEventListener("click", validateForm);
 function countBmi() {
   var p = [age.value, height.value, weight.value];
+  var gender = "";
+  var bmr;
   if (male.checked) {
     p.push("male");
+    gender = "male";
   } else if (female.checked) {
     p.push("female");
+    gender = "female";
+
   }
   form.reset();
   var bmi = Number(p[2]) / (((Number(p[1]) / 100) * Number(p[1])) / 100);
   var result = "";
-  if (bmi < 18.5) {
-    result = "Underweight";
-  } else if (18.5 <= bmi && bmi <= 24.9) {
-    result = "Healthy";
-  } else if (25 <= bmi && bmi <= 29.9) {
-    result = "Overweight";
-  } else if (30 <= bmi && bmi <= 34.9) {
-    result = "Obese";
-  } else if (35 <= bmi) {
-    result = "Extremely obese";
+  var suggested_calories = "";
+  if(gender == "male")
+    if (bmi < 18.5) {
+      result = "Underweight";
+      bmr = 66.47 + (13.75*pi[2]) + (5.003*pi[1]) - (6.755*pi[0]);
+      suggested_calories = bmr *1.55;
+    } else if (18.5 <= bmi && bmi <= 24.9) {
+      result = "Healthy";
+      bmr = 66.47 + (13.75*pi[2]) + (5.003*pi[1]) - (6.755*pi[0]);
+      suggested_calories = bmr *1.55;
+    } else if (25 <= bmi && bmi <= 29.9) {
+      result = "Overweight";
+      bmr = 66.47 + (13.75*pi[2]) + (5.003*pi[1]) - (6.755*pi[0]);
+      suggested_calories = bmr *1.55;
+    } else if (30 <= bmi && bmi <= 34.9) {
+      result = "Obese";
+      bmr = 66.47 + (13.75*pi[2]) + (5.003*pi[1]) - (6.755*pi[0]);
+      suggested_calories = bmr *1.55;
+    } else if (35 <= bmi) {
+      result = "Extremely obese";
+      bmr = 66.47 + (13.75*pi[2]) + (5.003*pi[1]) - (6.755*pi[0]);
+      suggested_calories = bmr *1.55;
+      
+    }
+
+  else if(gender == "female")
+    if (bmi < 18.5) {
+      result = "Underweight";
+      bmr = 655.1 + (9.563*pi[2]) + (1.850*pi[1]) - (4.676*pi[0]);
+      suggested_calories = bmr *1.55;
+    } else if (18.5 <= bmi && bmi <= 24.9) {
+      result = "Healthy";
+      bmr = 655.1 + (9.563*pi[2]) + (1.850*pi[1]) - (4.676*pi[0]);
+      suggested_calories = bmr *1.55;
+    } else if (25 <= bmi && bmi <= 29.9) {
+      result = "Overweight";
+      bmr = 655.1 + (9.563*pi[2]) + (1.850*pi[1]) - (4.676*pi[0]);
+      suggested_calories = bmr *1.55;
+    } else if (30 <= bmi && bmi <= 34.9) {
+      result = "Obese";
+      bmr = 655.1 + (9.563*pi[2]) + (1.850*pi[1]) - (4.676*pi[0]);
+      suggested_calories = bmr *1.55;
+    } else if (35 <= bmi) {
+      result = "Extremely obese";
+      bmr = 655.1 + (9.563*pi[2]) + (1.850*pi[1]) - (4.676*pi[0]);
+      suggested_calories = bmr *1.55;
+
+    }  
+  var link="";
+  if (result == "Underweight") {
+    link = "workout_planner_underweight.html";
+  } else if (result == "Healthy") {
+    link = "workout_planner_normal.html";;
+  } else if (result == "Overweight") {
+    link = "workout_planner_overweight.html";
+  } else if (result == "Obese") {
+    link = "workout_planner_obese.html";
   }
+
+  document.getElementById("myAnchor").href = link;
+
+
   var h1 = document.createElement("h1");
   var h2 = document.createElement("h2");
   var t = document.createTextNode(result);
   var b = document.createTextNode("BMI: ");
   var r = document.createTextNode(parseFloat(bmi).toFixed(2));
+  var s = document.createTextNode("Suggested Calories: ");
+  var c = document.createTextNode(parseFloat(suggested_calories).toFixed(2));
+
   h1.appendChild(t);
   h2.appendChild(b);
   h2.appendChild(r);
+  h2.appendChild(s);
+  h2.appendChild(c);
+  
   document.body.appendChild(h1);
   document.body.appendChild(h2);
+  document.body.appendChild(h2);
+
   document.getElementById("submit").removeEventListener("click", countBmi);
   document.getElementById("submit").removeEventListener("click", validateForm);
 }
---
+++ static/js/workout_planner.js
@@ -0,0 +1,174 @@
+const date = new Date();
+const selectedDate = new Date();
+
+const renderCalendar = () => {
+    
+const monthDays = document.querySelector('.days');
+
+const lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
+
+const prevLastDay = new Date(date.getFullYear(), date.getMonth(), 0).getDate();
+
+const convertDays = (day) => {
+
+   return (day === 0) ? 6 : day - 1
+}
+
+const firstDayIndex = convertDays(new Date(date.getFullYear(), date.getMonth(), 1).getDay());
+
+const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+]
+
+document.querySelector('.date h1').innerHTML = months[date.getMonth()];
+
+document.querySelector('.date p').innerHTML = selectedDate.toDateString();
+
+
+let days = "";
+let totalDays = 0;
+
+for(let x = firstDayIndex; x > 0; x--){
+    days += `<div class="prev-date">${prevLastDay - x + 1}</div>`
+    totalDays += 1;
+}
+
+for(let i = 1; i <= lastDay; i++){
+    let currentDate = date
+    currentDate.setDate(i)
+    currentDate = currentDate.toDateString()
+    let workout = JSON.parse(localStorage.getItem(currentDate))
+
+
+    if(i === new Date().getDate() && date.getMonth() === new Date().getMonth() && date.getFullYear() === new Date().getFullYear()){
+
+        if(date.toString() === selectedDate.toString()){
+            days += `<div class="selectedToday">${i}</div>`
+        }
+        else{
+            days += `<div today class="today">${i}</div>`
+        }
+        totalDays += 1;
+    }
+    else if(i === selectedDate.getDate() && date.getMonth() === selectedDate.getMonth() && date.getFullYear() === selectedDate.getFullYear()){
+        if(workout !== null){
+            days += `<div selectedSavedDay class="selectedSavedDay">${i}</div>`
+        }else{
+            days += `<div selectedDay class="selectedDay">${i}</div>` 
+        }
+        totalDays += 1;
+    }
+    else{
+        if(workout !== null){
+            days += `<div savedDay class="savedDay">${i}</div>`  
+        }else{
+            days += `<div day>${i}</div>`  
+        }
+        totalDays += 1;
+    }
+}
+
+for(let i = 1; totalDays < 42; i++){
+    days += `<div class="next-date">${i}</div>`
+    totalDays += 1;
+}
+monthDays.innerHTML = days;
+
+document.querySelectorAll('[day],[today],[savedDay]').forEach(day => {
+    day.addEventListener('click', () => {
+        selectedDate.setMonth(date.getMonth());
+        selectedDate.setFullYear(date.getFullYear());
+        selectedDate.setDate(day.innerHTML);
+        renderCalendar();
+    })
+})
+
+let workout = JSON.parse(localStorage.getItem(selectedDate.toDateString()))
+
+if(workout === null){
+    document.getElementById("set1").value = ""
+    document.getElementById("rep1").value = ""
+    document.getElementById("set2").value = ""
+    document.getElementById("rep2").value = ""
+    document.getElementById("set3").value = ""
+    document.getElementById("rep3").value = ""
+}else{
+    document.getElementById("set1").value = workout.workout1.name
+    document.getElementById("rep1").value = workout.workout1.weight
+    document.getElementById("set2").value = workout.workout2.name
+    document.getElementById("rep2").value = workout.workout2.weight
+    document.getElementById("set3").value = workout.workout3.name
+    document.getElementById("rep3").value = workout.workout3.weight 
+}
+
+
+}
+
+document.querySelector('.prev').addEventListener('click', () => {
+    date.setDate(1);
+    date.setMonth(date.getMonth()-1);
+    renderCalendar();
+});
+
+document.querySelector('.next').addEventListener('click', () =>{
+    date.setDate(1);
+    date.setMonth(date.getMonth()+1);
+    renderCalendar();
+});
+
+renderCalendar();
+
+document.querySelector('.Save').addEventListener('click', () => {
+    saveWorkout();
+})
+
+document.querySelector('.Delete').addEventListener('click', () => {
+    clearWorkout();
+})
+
+const saveWorkout = () => {
+
+const workout = {
+    date: selectedDate,
+    workout1: {
+        name: document.getElementById("set1").value,
+        weight: document.getElementById("rep1").value,
+    },
+    workout2: {
+        name: document.getElementById("set2").value,
+        weight: document.getElementById("rep2").value,
+    },
+    workout3: {
+        name: document.getElementById("set3").value,
+        weight: document.getElementById("rep3").value,
+    }
+};
+
+localStorage.setItem(selectedDate.toDateString(), JSON.stringify(workout, null, 2));    
+
+renderCalendar();
+};
+
+const clearWorkout = () => {
+    localStorage.removeItem(selectedDate.toDateString());
+    document.getElementById("set1").value = ""
+    document.getElementById("rep1").value = ""
+    document.getElementById("set2").value = ""
+    document.getElementById("rep2").value = ""
+    document.getElementById("set3").value = ""
+    document.getElementById("rep3").value = ""
+    renderCalendar();
+}
+
+
